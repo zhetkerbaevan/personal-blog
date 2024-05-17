111a82e0d75c9adb58c223d02b38df2d7d7f5dd2
Zhetkerbayeva Nazerke <zhetkerbayevann8@gmail.com>
2024-05-17 21:03:07 +0600

    succesfully connected to database postgresql

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,1 +1,29 @@
 package api
+
+import (
+	"database/sql"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	db *sql.DB
+	address string
+}
+
+func NewAPIServer(db *sql.DB, address string) *APIServer { 
+	//create new instance of APIServer struct
+	return &APIServer{
+		db : db,
+		address: address,
+	}
+}
+
+func (s *APIServer) Run() error {
+	//create new router
+	router := mux.NewRouter()
+	
+	//create subrouter (version of api)
+	subrouter := router.PathPrefix("/v1/api").Subrouter()
+	
+}
--- cmd/personal-blog/main.go
+++ cmd/personal-blog/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/zhetkerbaevan/personal-blog/internal/config"
+	"github.com/zhetkerbaevan/personal-blog/internal/db"
+)
+
+func main() {
+	db, err := db.NewPostgreSQLStorage(config.Config{
+		DBHost: config.Envs.DBHost,
+		DBPort: config.Envs.DBPort,
+		DBUser: config.Envs.DBUser,
+		DBName: config.Envs.DBName,
+		DBPassword: config.Envs.DBPassword,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to db", db)
 }
