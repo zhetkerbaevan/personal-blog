07a0d99284b7127d91edf96320af135aa672253f
Zhetkerbayeva Nazerke <zhetkerbayevann8@gmail.com>
2024-05-19 19:06:01 +0600

    created migrations, added user table, we can make request register and everything works

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,8 +2,12 @@ package api
 
 import (
 	"database/sql"
+	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/zhetkerbaevan/personal-blog/internal/service/user"
+	"github.com/zhetkerbaevan/personal-blog/internal/store"
 )
 
 type APIServer struct {
@@ -24,6 +28,12 @@ func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	
 	//create subrouter (version of api)
-	subrouter := router.PathPrefix("/v1/api").Subrouter()
-	
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userStore := store.NewUserStore(s.db)
+	userService := user.NewHandler(userStore)
+	userService.RegisterRoutes(subrouter)
+	log.Println("Listening on", s.address)
+
+	return http.ListenAndServe(s.address, router)
 }
---
+++ internal/models/user.go
@@ -0,0 +1,23 @@
+package models
+
+type User struct {
+	Id       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
+	Name     string `json:"name"`
+	Surname  string `json:"surname"`
+	Age      int    `json:"age"`
+}
+
+type RegisterUser struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+	Name     string `json:"name" validate:"required"`
+	Surname  string `json:"surname" validate:"required"`
+	Age      int    `json:"age" validate:"required"`
+}
+
+type UserStore interface {
+	CreateUser(User) error
+	GetUserByEmail(string) (*User, error)
+}
